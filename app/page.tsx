04bbaf7ff61f4009ac075d198223617cea726407
Piupiu-dot <michael.cunha.schilling@gmail.com>
2026-06-03 20:24:13 +0200

    feat: update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   }, [router])
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#faf8f5]">
-      <p className="text-[#b8924a]">Wird geladen...</p>
+      <div className="w8 h-8 border-2 border-[#b8924a] border-t-transparent rounded-full animate-spin"/>
     </div>
   )
 }
